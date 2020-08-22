b21d67ba8a363bfa81f2f8bab66b8a01f319ad36
Duncan Ryan <duncancryan1@gmail.com>
2020-08-22 18:55:43 +0100

    created list element for new games in the formhanlder, as well as paragraph elements for its details, paragraph elements appended to list element

--- js/app.js
+++ js/app.js
@@ -6,10 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const formHandler = function (event) {
   event.preventDefault();
+  
   const gameList = document.querySelector('#game-list');
+  
   const title = event.target.title.value;
   const studio = event.target.studio.value;
-  const metacriticScore = event.target.metacritic-score.value;
+  const metacritic = event.target.metacritic-score.value;
   const myScore = event.target.my-score.value;
   const genre = event.target.genre.value;
+
+  const newGame = document.createElement("li");
+  const titlePara = document.createElement("p");
+  const studioPara = document.createElement("p");
+  const metacriticPara = document.createElement("p");
+  const myScorePara = document.createElement("p");
+  const genrePara = document.createElement("p");
+
+  titlePara.textContent = title;
+  studioPara.textContent = studio;
+  metacriticPara.textContent = metacritic;
+  myScorePara.textContent = myScore;
+  genrePara.textContent = genre;
+
+  newGame.appendChild(titlePara);
+  newGame.appendChild(studioPara);
+  newGame.appendChild(metacriticPara);
+  newGame.appendChild(myScorePara);
+  newGame.appendChild(genrePara);
 };
